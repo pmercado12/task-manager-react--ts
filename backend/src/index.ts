9aa4740b172ccea966162727393271a10852eedb
pmercado12 <zafirapeter3@gmail.com>
2026-04-15 21:34:31 -0400

    version inicial de prisma funcionando

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,63 +1,88 @@
-const cors = require('cors');
-const express = require('express');
+
+import { prisma } from './lib/prisma.js' // Recuerda la extensión .js si usas ESM puro
+
+import cors from 'cors';
+import express from 'express';
 
 const app = express();
 const PORT = 3000;
+//dotenv.config();
+
 
 app.use(express.json());
 app.use(cors());
 
-let tasks=[
-    {id:1, text:'Study express',state:'completed'},
-    {id:2, text:'Study backend',state:'pending'}
-];
 
 app.get("/", (req:any,res:any)=>{
     res.send("backend is working");
 });
 
-app.get("/tasks", (req:any,res:any)=>{
-    res.json(tasks);
+app.get("/tasks", async (req:any,res:any)=>{
+    try {        
+        const tasks = await prisma.tasks.findMany();
+        res.json(tasks);
+    } catch (error) {
+        console.error("Error al obtener tareas:", error);
+        res.status(500).json({ error: "No se pudieron obtener las tareas" });
+    }     
 });
 
-app.post("/tasks", (req:any, res:any) => {    
-    const newTask = {
-        id: req.body.id,
-        text: req.body.text,
-        state: req.body.state
-    };
-    console.log('Tarea Registrada', newTask);
-    tasks.push(newTask);
-    res.json(newTask);
-});
+app.post("/tasks", async (req:any, res:any) => {    
+    
+    try {
+        const newTask = {
+            id: req.body.id,
+            text: req.body.text,
+            state: req.body.state
+        };
 
-app.delete("/tasks/:id", (req:any, res:any) => {    
-    const { id } = req.params;
-    const index = tasks.findIndex(task => task.id == id);
-
-    if (index !== -1) {
-        tasks.splice(index, 1);
-        res.status(200).json({ mensaje: 'Usuario eliminado correctamente' });
-    } else {
-        res.status(404).json({ mensaje: 'Usuario no encontrado' });
-    }
-});
-
-app.put('/tasks/:id', (req:any, res:any) => {
-    const { id } = req.params;
-    const { state } = req.body;
-
-    const task = tasks.find(u => u.id == id);
-
-    if (task) {
-        task.state = state;  // Actualiza el dato
-        res.status(200).json({
-            mensaje: 'Usuario actualizado',
-            task
+        const response = await prisma.tasks.create({
+            data: newTask,
         });
-    } else {
-        res.status(404).json({ mensaje: 'Usuario no encontrado' });
+
+        res.status(201).json(newTask);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Error al crear la tarea" });
+    }    
+});
+
+app.delete("/tasks/:id", async (req:any, res:any) => {    
+    try {
+        const { id } = req.params;
+
+        await prisma.tasks.delete({
+        where: { id: id },
+        });
+
+        // 204 significa "No Content", éxito pero sin nada que devolver
+        res.status(200).json("ok"); 
+    } catch (error) {
+        res.status(404).json({ error: "No se pudo eliminar la tarea" });
     }
+});
+
+app.put('/tasks/:id', async (req:any, res:any) => {
+    try {
+        const { id } = req.params;
+        const { text, state } = req.body;
+
+        const updatedTask = await prisma.tasks.update({
+        where: { id: id }, // Buscamos por el ID de la URL
+        data: {
+            //text: text,
+            state: state,
+        },
+        });
+
+        res.json(updatedTask);
+    } catch (error) {
+        // Prisma lanza un error si el ID no existe
+        res.status(404).json({ error: "Tarea no encontrada o error de datos" });
+    }
+
+
+
 });
 
 app.listen(PORT, ()=>{
@@ -74,4 +99,10 @@ curl.exe -X POST http://localhost:3000/tasks -H "Content-Type: application/json"
 npm install prisma @prisma/client
 
 npx prisma init
+
+docker run --name postgres-container \
+  -e POSTGRES_PASSWORD=123456 \
+  -p 5432:5432 \
+  -v postgres_data:/var/lib/postgresql \
+  -d postgres:16
 */
---
+++ backend/src/lib/prisma.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config';
+import { PrismaClient } from '@prisma/client'
+import { PrismaPg } from '@prisma/adapter-pg'
+import pg from 'pg'
+
+console.log(process.env.DATABASE_URL );
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL })
+
+pool.on('error', (err) => {
+  console.error('Error inesperado en el pool de pg:', err)
+})
+
+const adapter = new PrismaPg(pool)
+
+// Aquí es donde pasas el "adapter" que te pedía el error
+export const prisma = new PrismaClient({ adapter })
