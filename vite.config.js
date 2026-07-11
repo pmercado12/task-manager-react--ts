8fdefdd057c2bdb4b2d9040f0700b409d0ee9722
pmercado12 <zafirapeter3@gmail.com>
2026-07-10 22:05:50 -0400

    test: primer test de fecha

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+  },
   plugins: [react()],
 })
