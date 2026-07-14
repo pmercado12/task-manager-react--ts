d544af4f6eee10587c19da69ccbda5429b4603ce
pmercado12 <zafirapeter3@gmail.com>
2026-07-13 22:37:13 -0400

    test:parte1

---
+++ tests/flujo-tareas.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test'
+ 
+test('un usuario puede crear una tarea y verla en la lista', async ({ page }) => {
+  // 1. Entrar a la aplicación
+  await page.goto('/')
+ 
+  // 2. Crear una tarea
+  await page.getByPlaceholder('Escribe una tarea').fill('Comprar pan')
+  await page.getByRole('button', { name: 'Adicionar Tarea' }).click()
+ 
+  // 3. Verla en la lista
+  await expect(page.getByText('Comprar pan')).toBeVisible()
+})
